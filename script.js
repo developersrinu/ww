e192ffcba54096896e70dbd202da19c3d283a4ef
accio-f3 <acciof3@gmail.com>
2023-07-21 22:24:08 +0530

    2nd

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const tHeadRow = document.getElementById('table-heading-row');
 const tBody = document.getElementById('table-body');
+const boldButton=document.getElementById('bold-btn');
+const italicsButton = document.getElementById('italics-btn');
+const underlineButton = document.getElementById('underline-btn');
 
+let currentCell;
 const columns = 26;
 const rows = 100;
 
@@ -11,7 +15,7 @@ for (let col = 0; col < columns; col++) {
     tHeadRow.append(th);
 }
 
-for (let row = 1; row <= rows; row++) {
+for (let row = 1; row <= rows; row++) { // Row -> 1-100
     // i create a tr
     let tr=document.createElement('tr');
     // number cell
@@ -19,9 +23,51 @@ for (let row = 1; row <= rows; row++) {
     // injecting number in th
     th.innerText=row;
     tr.append(th);
-    for(let col=0;col<columns;col++){
+    for(let col=0;col<columns;col++){ //COL-> 0->26 // A->Z
         let td = document.createElement('td');
+        td.setAttribute('contenteditable','true');
+        // unique row and unique col
+        // ColRow
+        td.setAttribute('id',`${String.fromCharCode(col+65)}${row}`)
+        td.addEventListener('focus',(event)=>onFocusFn(event));
         tr.append(td);
     }
     tBody.append(tr);
 }
+
+function onFocusFn(event){
+    currentCell=event.target;
+    document.getElementById('current-cell').innerText=currentCell.id;
+    if(currentCell.style.fontWeight==='bold'){
+        boldButton.style.backgroundColor='yellow';
+    }
+    else boldButton.style.backgroundColor='transparent';
+}
+
+boldButton.addEventListener('click',()=>{
+    if(currentCell.style.fontWeight==='bold'){
+        currentCell.style.fontWeight='normal';
+    }
+    else {
+        currentCell.style.fontWeight='bold';
+        boldButton.style.backgroundColor='yellow';
+    }
+
+    // currentCell.style.fontWeight = currentCell.style.fontWeight==='bold'? 'normal':'bold';
+})
+
+italicsButton.addEventListener('click',()=>{
+    if(currentCell.style.fontStyle==='italic'){
+        currentCell.style.fontStyle='normal';
+    }
+    else currentCell.style.fontStyle='italic';
+})
+
+underlineButton.addEventListener('click',()=>{
+    if(currentCell.style.textDecoration==='underline'){
+        currentCell.style.textDecoration='none';
+    }
+    else currentCell.style.textDecoration='underline';
+})
+
+// sundays is very 
